a37e1eb0e28c9350f422068410f9f5f64e58b2d4
Alex Qian <qianalex8@gmail.com>
2022-10-17 21:04:43 +0000

    finished logic for main pages

--- src/Add.js
+++ src/Add.js
@@ -10,39 +10,54 @@ class Add extends Component {
             score: 0
         }
         
+        this.updateState = this.updateState.bind(this);
+        this.beginGame = this.beginGame.bind(this);
         this.checkAnswer = this.checkAnswer.bind(this);
-        this.generateNumbers = this.generateNumbers.bind(this);
+    }
+    
+    updateState(newNum1, newNum2, newScore) {
+        this.setState({ num1: newNum1, num2: newNum2, score: newScore });
+    }
+    
+    beginGame() {
+        this.updateState(this.generateRandomNumber(), this.generateRandomNumber(), 0);
+        this.clearAnswer();  
     }
     
     checkAnswer(event) {
-        if (+this.state.num1 + +this.state.num2 === +event.target[0].value) {
-            this.setState({ num1: this.state.num1, num2: this.state.num2, score: this.state.score++ });
-            console.log("CORRECT!");
-        }
-        else {
-            console.log("WRONG!");
-        }
-        this.generateNumbers();
         event.preventDefault();
+        
+        let currentScore = this.state.score;
+        
+        if (event.target[0].value !== "" && +this.state.num1 + +this.state.num2 === +event.target[0].value) {
+            currentScore++;
+        }
+        
+        this.updateState(this.generateRandomNumber(), this.generateRandomNumber(), currentScore);
+        this.clearAnswer();
     }
     
-    generateNumbers() {
-        this.setState({ num1: Math.floor(Math.random() * 10) + 1, num2: Math.floor(Math.random() * 10) + 1, score: this.state.score });
+    generateRandomNumber() {
+        return Math.floor(Math.random() * 10) + 1;
+    }
+    
+    clearAnswer() {
+        document.getElementById("answer-form").reset();
     }
     
     render() {
         return (
             <div>
                 <h1>Add</h1>
-                <button onClick={this.generateNumbers}>Begin!</button>
+                <button onClick={this.beginGame} className="begin-button">Begin!</button>
                 
                 <h1>{this.state.num1} + {this.state.num2} = ?</h1>
-                <form onSubmit={this.checkAnswer}>
+                <form id="answer-form" onSubmit={this.checkAnswer}>
                     <input type="text"/>
-                    <button type="submit">Check Answer</button>
+                    <button type="submit" className="check-button">Check Answer</button>
                 </form>
                 
-                <h1>Score: {this.state.score}</h1>
+                <h1 className="score">Score: {this.state.score}</h1>
             </div>
         );
     }
