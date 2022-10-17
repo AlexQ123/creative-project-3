cbb0bb088cddbeef9f668ccfcd22d47fcbe05ebf
Alex Qian <qianalex8@gmail.com>
2022-10-17 21:13:16 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Add from './Add';
 import Subtract from './Subtract';
+import Footer from './Footer';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar />
+      
       <div className="titles-container">
         <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/add" element={<Add/>} />
-        <Route path="/subtract" element={<Subtract/>} />
-      </Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/add" element={<Add/>} />
+          <Route path="/subtract" element={<Subtract/>} />
+        </Routes>
       </div>
+      
+      <Footer />
     </div>
   );
 }
