8672db920abaf22f61b7a7ea6b73273cc48eda5d
Alex Qian <qianalex8@gmail.com>
2022-10-17 19:01:59 +0000

    coded logic for add

--- src/Add.js
+++ src/Add.js
@@ -1,5 +1,51 @@
-export default function Add() {
-    return (
-        <h1>Add</h1>
-    );
+import React, {Component} from 'react';
+
+class Add extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            num1: 0,
+            num2: 0,
+            score: 0
+        }
+        
+        this.checkAnswer = this.checkAnswer.bind(this);
+        this.generateNumbers = this.generateNumbers.bind(this);
+    }
+    
+    checkAnswer(event) {
+        if (+this.state.num1 + +this.state.num2 === +event.target[0].value) {
+            this.setState({ num1: this.state.num1, num2: this.state.num2, score: this.state.score++ });
+            console.log("CORRECT!");
+        }
+        else {
+            console.log("WRONG!");
+        }
+        this.generateNumbers();
+        event.preventDefault();
+    }
+    
+    generateNumbers() {
+        this.setState({ num1: Math.floor(Math.random() * 10) + 1, num2: Math.floor(Math.random() * 10) + 1, score: this.state.score });
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Add</h1>
+                <button onClick={this.generateNumbers}>Begin!</button>
+                
+                <h1>{this.state.num1} + {this.state.num2} = ?</h1>
+                <form onSubmit={this.checkAnswer}>
+                    <input type="text"/>
+                    <button type="submit">Check Answer</button>
+                </form>
+                
+                <h1>Score: {this.state.score}</h1>
+            </div>
+        );
+    }
 }
+
+export default Add;
